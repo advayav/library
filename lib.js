23a3ee6f6231841b537d85ff1aa7c4ac67412cc9
advayav <advaya.venbakkam@gmail.com>
2025-07-14 23:28:01 -0400

    added the logic for book display+button

--- lib.js
+++ lib.js
@@ -1,8 +1,10 @@
+const myLibrary = []
+
 function Book(title, author, pages, read) {
     if (!new.target) {
         return "You must use new to call the constructor"
     }
-
+    this.id = crypto.randomUUID()
     this.title = title
     this.author = author
     this.pages = pages
@@ -12,5 +14,30 @@ function Book(title, author, pages, read) {
     }
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 259, "not read yet")
-console.log(theHobbit.info())
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 259, "not read yet")
+// console.log(theHobbit.info())
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+}
+
+function displayBooks() {
+    const books = document.querySelector(".books")
+    books.innerHTML = ''
+
+    myLibrary.forEach(book => {
+        const info = document.createElement('div')
+        info.className = 'book-tile'
+        info.innerHTML = `
+            <p class="title">${book.title}</p>
+            <p class="author">Author: ${book.author}</p>
+            <p class="pages">Pages: ${book.pages}</p>
+            <p class="read">Status: ${book.read}</p>`
+        
+        books.appendChild(info)
+    })
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 259, "not read yet")
+displayBooks()
